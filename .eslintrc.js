4cecde92c4ea50cd7d7ca5733e3d67e1a2680cca
Mahedi Hasan <mahedi.hs1997@gmail.com>
2021-01-31 21:57:38 +0000

    feat(main): creating intial project :tada:

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['simple-import-sort'],
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'simple-import-sort/imports': 'error',
+  },
+};
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  const params = req.params;
+
+  res.send(`Hello World! ${JSON.stringify(params)}`);
+});
+
+app.get('/other', (_, res) => {
+  res.send(`I am in a different route! `);
+});
+
+const server = app.listen(8080, () => {
+  const addr = server.address();
+  const binding =
+    typeof addr === 'string' ? `pipe/socket ${addr}` : `port ${addr.port}`;
+
+  console.log(`Server listening on binding ${binding}`);
+});
